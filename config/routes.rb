94947cf9949b6518a0650f548296da9c1b90cbbd
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-05-14 13:34:57 -0400

    Add CategoriesController, Show view, and related initial tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Website::Application.routes.draw do
   root :to => 'static_page#index'
+  
+  match '/:slug' => 'categories#show'
 end
---
+++ db/migrate/20120511093303_add_slug_to_category.rb
@@ -0,0 +1,4 @@
+class AddSlugToCategory < ActiveRecord::Migration
+  def change
+  end
+end
