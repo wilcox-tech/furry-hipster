f1aece199033d7858f3bc85f3e0a8722e9864531
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-06-19 10:37:19 -0400

    Add sidebar with pages.  Fixes #4 but needs major help with styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def markdownify(text)
     BlueCloth.new(text).to_html.html_safe
   end
+  
+  def make_sidebar_for(product)
+    sidebar = '<ul class="nav nav-list">'
+    sidebar += "<li>#{link_to product.title, category_product_path(product.category.slug, product.slug)}</li>"
+    product.detail_pages.each {|page| sidebar += "<li>#{link_to page.title, category_product_detail_page_path(product.category.slug, product.slug, page.slug)}</li>"}
+    sidebar += "</ul>"
+    sidebar.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Website::Application.routes.draw do
   
   match '/:slug' => 'categories#show', :as => :category
   match '/:category/:slug' => 'products#show', :as => :category_product
-  match '/:category/:product/:slug' => 'detail_pages#show', :as => :category_product_detail
+  match '/:category/:product/:slug' => 'detail_pages#show', :as => :category_product_detail_page
 end
