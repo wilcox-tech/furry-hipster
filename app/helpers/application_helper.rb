eb299cfba01aabd2616857563f861b8fea13b44c
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-07-10 01:50:50 -0400

    Use active_link_to for Product sidebars.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
   
   def make_sidebar_for(product)
     sidebar = '<ul class="nav nav-list">'
-    sidebar += "<li>#{link_to product.title, category_product_path(product.category.slug, product.slug)}</li>"
-    product.detail_pages.each {|page| sidebar += "<li>#{link_to page.title, category_product_detail_page_path(product.category.slug, product.slug, page.slug)}</li>"}
+    sidebar += "#{active_link_to product.title, category_product_path(product.category.slug, product.slug), :active => :exclusive, :wrap_tag => :li}"
+    product.detail_pages.each {|page| sidebar += "#{active_link_to page.title, category_product_detail_page_path(product.category.slug, product.slug, page.slug), :wrap_tag => :li}"}
     sidebar += "</ul>"
     sidebar.html_safe
   end
